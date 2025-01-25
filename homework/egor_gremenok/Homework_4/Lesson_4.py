e228d838b234edd97b912167286d981a498912c6
e-gremenok <egor.gremenok@gmail.com>
2025-01-26 01:54:34 +0300

    Removed empty space after adding a tuple element. Homework_4 is done

--- homework/egor_gremenok/Homework_4/Lesson_4.py
+++ homework/egor_gremenok/Homework_4/Lesson_4.py
@@ -18,7 +18,7 @@ print(my_dict['tuple'][-1])
 my_dict['list'].append('last_element')
 my_dict['list'].pop(1)
 
-my_dict['dict'][('i am a tuple', )] = '2025'
+my_dict['dict'][('i am a tuple',)] = '2025'
 my_dict['dict'].pop('f_string')
 
 my_dict['set'].add('abracadabra')
