e0c827263f4edf89c435631afee6aa7bd5669734
e-gremenok <egor.gremenok@gmail.com>
2025-01-26 02:01:44 +0300

    Changed brackets intendation. Homework_4 is done

--- homework/egor_gremenok/Homework_4/Lesson_4.py
+++ homework/egor_gremenok/Homework_4/Lesson_4.py
@@ -1,16 +1,16 @@
 my_dict = {
-        'tuple': (42, 'test', True, None, 36.6),
-        'list': ['Guido', 1990, 3.14, False, 'Flack'],
-        'dict':
-            {
-                'int': 123,
-                'f_string': 'QWERTY',
-                'float': 640.0,
-                'boolean': True,
-                'l_string': 'Monty'
-            },
-        'set': {True, 2, 3.0, 'Quattro', 'V'}
-    }
+    'tuple': (42, 'test', True, None, 36.6),
+    'list': ['Guido', 1990, 3.14, False, 'Flack'],
+    'dict':
+        {
+            'int': 123,
+            'f_string': 'QWERTY',
+            'float': 640.0,
+            'boolean': True,
+            'l_string': 'Monty'
+        },
+    'set': {True, 2, 3.0, 'Quattro', 'V'}
+}
 
 print(my_dict['tuple'][-1])
 
