a5d519e6cc289a1b5ec72f42c8ddb122971989b1
e-gremenok <egor.gremenok@gmail.com>
2025-01-29 00:56:27 +0300

    Added extra blank lines before and after function definition in 7_3.py. Removed whitespace before ( in function definition in 7_3.py. Homework 7 is done.

--- homework/egor_gremenok/Homework_7/7_3.py
+++ homework/egor_gremenok/Homework_7/7_3.py
@@ -5,10 +5,12 @@ str_4 = 'результат: 2'
 
 all_str = [str_1, str_2, str_3, str_4]
 
-def change_num (text):
+
+def change_num(text):
     not_num = text[text.find(':') + 2:]
     num = int(not_num) + 10  # Можно было объединить с предыдущим, но, как по мне, так проще читать код
     print(num)
+
 
 for i in range(len(all_str)):
     change_num(all_str[i])
